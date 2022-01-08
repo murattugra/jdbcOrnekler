03403a65c9a4d26ebc2d8dba05e990575ad29552
Murat Arslan <mrterkmglsv@gmail.com>
2022-01-08 21:23:36 +0100

    jdbc

---
+++ src/main/java/techproed/jdbcOrnekler/Jdbc1Query01.java
@@ -0,0 +1,28 @@
+package techproed.jdbcOrnekler;
+
+public class Jdbc1Query01 {
+
+	public static void main(String[] args) {
+		
+		
+		// 1) ilgili driver i yuklemeliyiz, yani Tv nin fisini tak
+		
+		String isim="Ayse";
+		
+		
+		
+		System.out.println(isim);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
